22bb78377332b6fc1a4117576a4a5c5cb79b3552
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-08-28 13:07:34 +0100

    feat: implement total price in cart

--- components/cart/Summary.tsx
+++ components/cart/Summary.tsx
@@ -1,4 +1,15 @@
+import { useStore } from "@/context/StoreContext";
+
 export default function Summary() {
+  const { cart } = useStore();
+
+  const cartTotal = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+  const deliveryFees = 5;
+  const total = cartTotal + deliveryFees;
+
   return (
     <section className="md:flex-1 lg:max-w-150 p-8 md:p-12">
       <div className="flex flex-col gap-8 border border-[hsla(52,98%,53%,1)] p-4 rounded-2xl">
@@ -8,22 +19,22 @@ export default function Summary() {
 
         <div className="flex flex-col gap-4 border-b border-b-[hsla(52,98%,53%,1)] pb-2">
           <div className="font-bold flex items-center justify-between">
-            <p>Item’s total (1)</p>
-            <p>$320</p>
+            <p>Item’s total ({cart.length})</p>
+            <p>${cartTotal}</p>
           </div>
           <div className="font-bold flex items-center justify-between">
             <p>Delivery fees</p>
-            <p>$5</p>
+            <p>${deliveryFees}</p>
           </div>
         </div>
 
         <div className=" font-bold flex items-center justify-between">
           <p className="text-xl">Total</p>
-          <p>$325</p>
+          <p>${total}</p>
         </div>
 
         <button className="text-xl font-bold bg-[hsla(52,98%,53%,1)] text-black p-4 rounded-md cursor-pointer">
-          CHECKOUT ($325)
+          CHECKOUT (${total})
         </button>
       </div>
     </section>
